4bc8762099b88ad88f426b814e1603cab9de0df5
Carlos Diestro <cardiest@gmail.com>
2021-03-24 19:14:51 +0100

    Add files compression

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import compression from 'compression';
 import path from 'path';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
@@ -7,6 +8,7 @@ import App from '../client/components/App';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(compression());
 app.use('/assets', express.static(path.join(__dirname, '/assets')));
 
 app.get('/', (request, response) => {
